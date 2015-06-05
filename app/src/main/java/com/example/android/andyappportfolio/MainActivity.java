17fae644e90a3a99996324b9a5ce8f9609b7fed0
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-06-05 23:52:40 +0200

    Changed application theme to Holo

--- app/src/main/java/com/example/android/andyappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/andyappportfolio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.andyappportfolio;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
