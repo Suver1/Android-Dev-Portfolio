9ed8dee0a3d869417ccd922506aac72ef86f92cd
Andreas Hellesvik <andreashellesvik@hotmail.com>
2015-06-08 22:21:23 +0200

    Added Spotify Web API Lib

--- app/src/main/java/com/example/android/andyappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/andyappportfolio/MainActivity.java
@@ -2,16 +2,9 @@ package com.example.android.andyappportfolio;
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-
-import com.squareup.picasso.Picasso;
-
 
 public class MainActivity extends Activity {
 
@@ -25,7 +18,6 @@ public class MainActivity extends Activity {
         int btnNameId = 0;
         switch(view.getId()) {
             case R.id.mediaStreamerBtn:
-                btnNameId = R.string.media_streamer;
                 break;
             case R.id.superDuo1Btn:
                 btnNameId = R.string.super_duo1;
